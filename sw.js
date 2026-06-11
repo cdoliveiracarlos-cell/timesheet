161090e24202ca9abe46f3e191d968e2a4064aaa
cdoliveiracarlos-cell <cdoliveira.carlos@gmail.com>
2026-06-11 20:46:52 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'timesheet-v1';
+const ASSETS = [
+  '/timesheet/',
+  '/timesheet/index.html',
+  '/timesheet/manifest.json',
+  '/timesheet/icon-192.png',
+  '/timesheet/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
